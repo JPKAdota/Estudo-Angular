3df2fb8b7dc25df08df03d9d6058f03754b8cd8f
Kadota <joaokadota@gmail.com>
2022-07-18 20:57:44 -0300

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   angular.json
    	modified:   src/app/app.component.html
    	modified:   src/app/app.component.ts
    	modified:   src/app/app.module.ts
    	new file:   src/app/course/course-list.componet.html
    	new file:   src/app/course/course-list.componet.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseListComponent } from './course/course-list.componet';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/course/course-list.componet.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component ({
+    selector: 'app-course-list',
+    templateUrl: './course-list.componet.html'
+})
+
+export class  CourseListComponent {
+}
