640fb3bfa4366bc63e56dd1d61f880fcdaa6869f
Kadota <joaokadota@gmail.com>
2022-07-20 21:56:27 -0300

    modified:   angular.json 	new file:   src/app/course/course-info.component.html 	new file:   src/app/course/course-info.component.ts 	modified:   src/app/course/course-list.component.html 	modified:   src/app/course/course-list.component.ts 	new file:   src/app/course/course.module.ts 	new file:   src/app/course/course.service.ts 	modified:   src/app/course/course.ts 	modified:   src/styles.css

--- src/app/course/course-list.component.ts
+++ src/app/course/course-list.component.ts
@@ -1,36 +1,52 @@
-import { Component, OnInit } from "@angular/core";
-import { Course } from "./course";
+import { Component, OnInit } from '@angular/core';
+import { Course } from './course';
+import { CourseService } from './course.service';
 
-@Component ({
-    selector: 'app-course-list',
+@Component({
     templateUrl: './course-list.component.html'
 })
+export class CourseListComponent implements OnInit { 
 
-export class  CourseListComponent implements OnInit {
-    cousers: Course[] = [];
+    filteredCourses: Course[] = [];
 
-    ngOnInit(): void{
-        this.cousers = [{
-            id: 1,
-            name: 'Angular: forms',
-            imageURl: '',
-            price: 99.99,
-            code: 'XPS-8796',
-            duration: 120,
-            rating: 4.5,
-            releaseDate: 'December, 2 , 2019',
-        },
-        {
-            id: 2,
-            name: 'Angular: forms',
-            imageURl: '',
-            price: 45.99,
-            code: 'LKL-1094',
-            duration: 80,
-            rating: 4,
-            releaseDate: 'December, 6 , 2019'
+    _courses: Course[] = [];
+    
+    _filterBy: string;
 
-        },
-        ]
+    constructor(private courseService: CourseService) { }
+
+    ngOnInit(): void { 
+        this.retrieveAll();
     }
+
+    retrieveAll(): void { 
+        this.courseService.retrieveAll().subscribe({
+            next: courses => {
+                this._courses = courses;
+                this.filteredCourses = this._courses;
+            },
+            error: err => console.log('Error', err) 
+        })
+    }
+
+    deleteById(courseId: number): void { 
+        this.courseService.deleteById(courseId).subscribe({
+            next: () => { 
+                console.log('Deleted with success');
+                this.retrieveAll();
+            },
+            error: err => console.log('Error', err)
+        })
+    }
+
+    set filter(value: string) { 
+        this._filterBy = value;
+
+        this.filteredCourses = this._courses.filter((course: Course) => course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1);
+    }
+
+    get filter() { 
+        return this._filterBy;
+    }
+
 }
--- src/app/course/course.ts
+++ src/app/course/course.ts
@@ -1,10 +1,11 @@
-export class Course {
-    id!: number;
-    name!: string;
-    imageURl!: string;
-    price!: number;
-    code!: string;
-    duration!: number;
-    rating!: number;
-    releaseDate!: string;
+export class Course { 
+    id: number;
+    name: string;
+    imageUrl: string;
+    price: number;
+    code: string;
+    duration: number;
+    rating: number;
+    releaseDate: string;
+    description: string;
 }
