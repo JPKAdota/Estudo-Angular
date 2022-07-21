1fd21a1dfb44147520abcb470fd32d7086bb2100
Kadota <joaokadota@gmail.com>
2022-07-21 09:06:50 -0300

    modified:   src/app/course/course-list.component.html 	modified:   src/app/course/course-list.component.ts

--- src/app/course/course-list.component.ts
+++ src/app/course/course-list.component.ts
@@ -8,11 +8,27 @@ import { CourseService } from "./course.service";
 })
 
 export class  CourseListComponent implements OnInit {
-    cousers: Course[] = [];
+    _courses: Course[] = [];
+
+    _filterBy!: string;
+
+    _filteredCourses: Course[]=[];
     
     constructor(private courseService: CourseService) { }
 
     ngOnInit(): void{
-        this.cousers = this.courseService.retrieveAll();
+        this._courses = this.courseService.retrieveAll();
+        
+        this._filteredCourses = this._courses;
+    }
+
+    set filter(value: string) {
+        this._filterBy = value;
+
+        this._filteredCourses = this._courses.filter((course: Course) => course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase())>-1)
+    }
+
+    get filter() { 
+        return this._filterBy;
     }
 }
