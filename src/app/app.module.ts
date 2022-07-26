2567ed5699d391f7197b52c292d5699bf7ca68bb
Kadota <joaokadota@gmail.com>
2022-07-26 08:22:30 -0300

    modified:   src/app/app.component.html 	modified:   src/app/app.module.ts 	modified:   src/app/course/course-info.component.ts 	modified:   src/app/course/course-list.component.html 	modified:   src/app/course/course-list.component.ts 	new file:   src/app/nav-bar/nav-bar.component.html 	new file:   src/app/nav-bar/nav-bar.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './course/course-list.component';
 import { StarComponent } from './star/star.component';
 import { ReplacePipe } from './pipe/replace.pipe';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
     StarComponent,
-    ReplacePipe
+    ReplacePipe,
+    NavBarComponent 
+  ,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      },
+      {
+        path: 'courses', component: CourseListComponent
+      }
+    ])
+
   ],
   providers: [],
   bootstrap: [AppComponent]
