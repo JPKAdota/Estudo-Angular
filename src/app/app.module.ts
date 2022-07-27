b15eba2079217ec2c1deb42667a45d97fac9755c
Kadota <joaokadota@gmail.com>
2022-07-26 23:24:40 -0300

    modified:   src/app/app.module.ts 	renamed:    src/app/erro-404/error-404.component.html -> src/app/core/component/erro-404/error-404.component.html 	renamed:    src/app/erro-404/error-404.component.ts -> src/app/core/component/erro-404/error-404.component.ts 	renamed:    src/app/nav-bar/nav-bar.component.html -> src/app/core/component/nav-bar/nav-bar.component.html 	renamed:    src/app/nav-bar/nav-bar.component.ts -> src/app/core/component/nav-bar/nav-bar.component.ts 	new file:   src/app/core/core.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,50 +1,32 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { CourseListComponent } from './course/course-list.component';
-import { StarComponent } from './shared/component/star/star.component';
-import { ReplacePipe } from './pipe/replace.pipe';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { Error404Component } from './erro-404/error-404.component';
-import { CourseInfoComponent } from './course/course-info.component';
+import { CourseModule } from './course/course.module';
+import { CoreModule } from './core/core.module';
+import { Error404Component } from './core/component/erro-404/error-404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent,
-    StarComponent,
-    ReplacePipe,
-    NavBarComponent,
-    Error404Component,
-    CourseInfoComponent
-  ,
+    Error404Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
       {
-        path: 'courses', component: CourseListComponent
-      },
-      {
-        path: 'courses/info/:id', component: CourseInfoComponent
-      },
-      {
         path: '', redirectTo: 'courses', pathMatch: 'full'
-      },
-      {
+      }, {
         path: '**', component: Error404Component
       }
-    ])
-
+    ]),
+    CourseModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
